c9b1b06158881ac46cef80a463a4646c795741f3
Love Larsson <love.j.larsson@gmail.com>
2025-08-10 00:55:48 +0200

    Add smooth scroll + GSAP

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './style.css'
+import { gsap } from "gsap";
+    
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(ScrambleTextPlugin,ScrollTrigger,ScrollSmoother,SplitText,TextPlugin);
+
+ScrollSmoother.create({
+  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
